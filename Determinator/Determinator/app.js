faeadd625a46e680c4dffa5ea25c47361b6e62d5
Zarak <zarak@hotmail.ca>
2019-04-02 18:11:22 +0500

    Refactor

--- Determinator/Determinator/app.js
+++ Determinator/Determinator/app.js
@@ -1,8 +1,5 @@
-const { BotFrameworkAdapter, MemoryStorage } = require('botbuilder');
+const { ActivityTypes, BotFrameworkAdapter, MemoryStorage } = require('botbuilder');
 const restify = require('restify');
-const { ActivityTypes } = require('botbuilder');
-
-const inMemoryStorage = new MemoryStorage();
 
 class EchoBot {
     /**
@@ -19,17 +16,18 @@ class EchoBot {
     }
 }
 
-let server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
-});
-
+const inMemoryStorage = new MemoryStorage();
 const adapter = new BotFrameworkAdapter({
     appId: process.env.MicrosoftAppID,
     appPassword: process.env.MicrosoftAppPassword
 });
 
 const bot = new EchoBot();
+
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log(`\n${ server.name } listening to ${ server.url }`);
+});
 
 // Listen for incoming activities and route them to your bot main dialog.
 server.post('/api/messages', (req, res) => {
