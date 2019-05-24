e059fce8f348c38b97ee162ca178d0daf4e64dd5
Zarak <zarak@hotmail.ca>
2019-05-24 12:15:46 +0500

    Remove unnecessary steps

--- typescript/corebot/lib/dialogs/mainDialog.js
+++ typescript/corebot/lib/dialogs/mainDialog.js
@@ -25,9 +25,7 @@ class MainDialog extends botbuilder_dialogs_1.ComponentDialog {
         this.addDialog(new botbuilder_dialogs_1.TextPrompt('TextPrompt'))
             .addDialog(new clusteringDialog_1.ClusteringDialog(CLUSTERING_DIALOG, endpoint, conversationState))
             .addDialog(new botbuilder_dialogs_1.WaterfallDialog(MAIN_WATERFALL_DIALOG, [
-            this.introStep.bind(this),
             this.actStep.bind(this),
-            this.finalStep.bind(this),
         ]));
         this.initialDialogId = MAIN_WATERFALL_DIALOG;
     }
@@ -48,16 +46,6 @@ class MainDialog extends botbuilder_dialogs_1.ComponentDialog {
         });
     }
     /**
-     * First step in the waterfall dialog. Prompts the user for a command.
-     * Currently, this expects a booking request, like "book me a flight from Paris to Berlin on march 22"
-     * Note that the sample LUIS model will only recognize Paris, Berlin, New York and London as airport cities.
-     */
-    introStep(stepContext) {
-        return __awaiter(this, void 0, void 0, function* () {
-            return yield stepContext.prompt('TextPrompt', { prompt: 'What can I help you with today?\nSay something like "Book a flight from Paris to Berlin on March 22, 2020"' });
-        });
-    }
-    /**
      * Second step in the waterall.  This will use LUIS to attempt to extract the origin, destination and travel dates.
      * Then, it hands off to the bookingDialog child dialog to collect any remaining details.
      */
@@ -66,26 +54,6 @@ class MainDialog extends botbuilder_dialogs_1.ComponentDialog {
             // In this sample we only have a single intent we are concerned with. However, typically a scenario
             // will have multiple different intents each corresponding to starting a different child dialog.
             return yield stepContext.beginDialog('clusteringDialog');
-        });
-    }
-    /**
-     * This is the final step in the main waterfall dialog.
-     * It wraps up the sample "book a flight" interaction with a simple confirmation.
-     */
-    finalStep(stepContext) {
-        return __awaiter(this, void 0, void 0, function* () {
-            // If the child dialog ("bookingDialog") was cancelled or the user failed to confirm, the Result here will be null.
-            if (stepContext.result) {
-                // Now we have all the booking details.
-                // This is where calls to the booking AOU service or database would go.
-                // If the call to the booking service was successful tell the user.
-                const msg = `I have you booked to ${stepContext.result.destination} from ${stepContext.result.origin}.`;
-                yield stepContext.context.sendActivity(msg);
-            }
-            else {
-                yield stepContext.context.sendActivity('Thank you.');
-            }
-            return yield stepContext.endDialog();
         });
     }
 }
--- typescript/corebot/src/dialogs/mainDialog.ts
+++ typescript/corebot/src/dialogs/mainDialog.ts
@@ -32,9 +32,7 @@ export class MainDialog extends ComponentDialog {
         this.addDialog(new TextPrompt('TextPrompt'))
             .addDialog(new ClusteringDialog(CLUSTERING_DIALOG, endpoint, conversationState))
             .addDialog(new WaterfallDialog(MAIN_WATERFALL_DIALOG, [
-                this.introStep.bind(this),
                 this.actStep.bind(this),
-                this.finalStep.bind(this),
             ]));
 
         this.initialDialogId = MAIN_WATERFALL_DIALOG;
@@ -57,42 +55,12 @@ export class MainDialog extends ComponentDialog {
     }
 
     /**
-     * First step in the waterfall dialog. Prompts the user for a command.
-     * Currently, this expects a booking request, like "book me a flight from Paris to Berlin on march 22"
-     * Note that the sample LUIS model will only recognize Paris, Berlin, New York and London as airport cities.
-     */
-    private async introStep(stepContext: WaterfallStepContext): Promise<DialogTurnResult> {
-        return await stepContext.prompt('TextPrompt', { prompt: 'What can I help you with today?\nSay something like "Book a flight from Paris to Berlin on March 22, 2020"' });
-    }
-
-    /**
      * Second step in the waterall.  This will use LUIS to attempt to extract the origin, destination and travel dates.
      * Then, it hands off to the bookingDialog child dialog to collect any remaining details.
      */
     private async actStep(stepContext: WaterfallStepContext): Promise<DialogTurnResult> {
         // In this sample we only have a single intent we are concerned with. However, typically a scenario
         // will have multiple different intents each corresponding to starting a different child dialog.
-
         return await stepContext.beginDialog('clusteringDialog');
-    }
-
-    /**
-     * This is the final step in the main waterfall dialog.
-     * It wraps up the sample "book a flight" interaction with a simple confirmation.
-     */
-    private async finalStep(stepContext: WaterfallStepContext): Promise<DialogTurnResult> {
-        // If the child dialog ("bookingDialog") was cancelled or the user failed to confirm, the Result here will be null.
-        if (stepContext.result) {
-            // Now we have all the booking details.
-
-            // This is where calls to the booking AOU service or database would go.
-
-            // If the call to the booking service was successful tell the user.
-            const msg = `I have you booked to ${ stepContext.result.destination } from ${ stepContext.result.origin }.`;
-            await stepContext.context.sendActivity(msg);
-        } else {
-            await stepContext.context.sendActivity('Thank you.');
-        }
-        return await stepContext.endDialog();
     }
 }
